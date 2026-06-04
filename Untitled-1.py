6f1721dca528a83b7f9a426f5c50325dd8bdec80
marlene-diaz <marlenequizhpi11@gmail.com>
2026-06-03 20:11:10 -0500

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str):
+# each letter in magazine can be used only once in random note
+
+    # breakdown randsom note into a list/ dictionary , how many it appears in randsom note
+    
+    # if statement if list"a" appears for all in magaize then return true else false
